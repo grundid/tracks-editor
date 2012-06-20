dc4f64196a8ce93b3ec184238a15fb8d0177e76f
Adrian Stabiszewski <as@grundid.de>
2012-06-20 23:04:46 +0200

    added tr language

---
+++ src/main/webapp/resources/messages-tr.js
@@ -0,0 +1,42 @@
+var MSG = {
+	button_save : "Kurtarmak",
+	button_cancel : "Iptal",
+	button_add_tag : "+ Tag",
+	button_josm : "JOSM",
+	button_add_tag_title : "Yeni etiket ekle",
+	button_josm_title : "JOSM mevcut nesnesini açın",
+
+	title_change_properties : "özelliklerini değiştirme",
+	tag_addr_street : "sokak",
+	tag_addr_housenumber : "ev numarasına",
+	tag_addr_postcode : "posta kodu",
+	tag_addr_city : "şehir",
+	tag_addr_country : "ülke",
+
+	menu_login : "giriş",
+
+	prompt_tag_name : "Etiket adını girin:",
+	prompt_changeset : "Changeset için bir yorum girin:",
+
+	info_upload_successful : "Başarılı yükleme",
+	info_no_object_found : "Sorunlu nesneler bulunmadı. Bu iyi tebrikler!",
+	info_tokens_deleted : "OAuth jetonları silindi",
+	info_tokens_received : "Yeni OAuth jetonları geldi",
+	info_tokens_available : "OAuth jetonları kullanılabilir",
+
+	objects_found : "Nesneler bulundu",
+	warn_no_localstorage : "Tarayıcınız yerel depolama desteklemiyor. Herhangi bir değişiklik yapmak mümkün olmayacaktır.",
+	warn_no_changes : "Yükleme için değişiklikler uygundur",
+	warn_zoom : "Veri indirmek için biraz daha fazla yakınlaştırma olun. Şu yakınlaştırma düzeyi",
+	warn_unsaved_changes : "Sen kaydedilmemiş değişiklikler var. Yeni veri indirerek değişiklikler kaybolur.",
+	error : "Hata",
+	error_download : "Muhtemelen mevcut alan sunucu için çok büyük. Biraz yakınlaştırmak veya tarayıcı pencerenizin boyutunu azaltmak için \ ntry.",
+	error_jsom : "Muhtemelen mevcut Alan SUNUCU for Eklendi çok büyük boy. Biraz yakınlaştırmak veya Tarayici pencerenizin boyutunu azaltmak for \ ntry.",
+	error_no_tokens : "Lütfen önce OSM'ye giriş yapın",
+
+	error_geo_no_access : "Erişime izin verilmedi",
+	error_geo_no_position : "Pozisyona karar verilmedi",
+	error_geo_timeout : "120 saniyede bir sonuç çıkmadı. Lütfen tekrar deneyiniz.",
+	error_geo_no_api : "Kein GEO Location support"
+
+};
