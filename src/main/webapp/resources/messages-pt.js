3ea89e5851b9caefa882e7fd88328bc8105fb5fe
Adrian Stabiszewski <as@grundid.de>
2012-09-11 22:37:04 +0200

    PT and RO translations added

---
+++ src/main/webapp/resources/messages-pt.js
@@ -0,0 +1,42 @@
+var MSG = {
+	button_save : "Guardar",
+	button_cancel : "Cancelar",
+	button_add_tag : "+ etiqueta",
+	button_josm : "JOSM",
+	button_add_tag_title : "Adicionar nova etiqueta",
+	button_josm_title : "Abrir objeto atual no JOSM",
+
+	title_change_properties : "Alterar propriedades",
+	tag_addr_street : "Rua",
+	tag_addr_housenumber : "Número de porta",
+	tag_addr_postcode : "Código postal",
+	tag_addr_city : "Cidade",
+	tag_addr_country : "País",
+
+	menu_login : "Entrar",
+
+	prompt_tag_name : "Por favor indique o nome da etiqueta:",
+	prompt_changeset : "Por favor indique um comentário para as alterações:",
+
+	info_upload_successful : "Enviado com sucesso",
+	info_no_object_found : "Não foram encontrados objetos com problemas. Isto é bom. Parabéns!",
+	info_tokens_deleted : "Símbolos OAuth removidos",
+	info_tokens_received : "Novos símbolos OAuth recebidos",
+	info_tokens_available : "Símbolos OAuth disponíveis. <br>Por favor leia <strong>Acerca de</strong> para mais informações.",
+
+	objects_found : "objetos encontrados",
+	warn_no_localstorage : "O seu navegador não suporta armazenamento local. Se poderá efetuar qualquer alteração.",
+	warn_no_changes : "Não existem alterações disponíveis para enviar",
+	warn_zoom : "Por favor amplie um pouco mais para carregar os dados. Nível de ampliação atual",
+	warn_unsaved_changes : "Não guardou as alterações. Ao carregar novos dados as suas alterações serão perdidas",
+	error : "Erro",
+	error_download : "Provavelmente a área atual é muito grande para o servidor.\nTente ampliar um pouco mais ou reduzir o tamanho da janela do seu navegador.",
+	error_jsom : "Existe um problema com as permissões do JavaScript.\nTenha a certeza de que pode executar scipts no domínio local. NoScript ativo?",
+	error_no_tokens : "Por favor entre primeiro com o OSM.",
+
+	error_geo_no_access : "Acesso não autorizado.",
+	error_geo_no_position : "A posição não pode ser determinada.",
+	error_geo_timeout : "Sem resultados após 120 segundos. Por favor tente de novo.",
+	error_geo_no_api : "Sem suporte para GEO Location"
+
+};
---
+++ src/main/webapp/resources/messages-ro.js
@@ -0,0 +1,42 @@
+var MSG = {
+	button_save : "Salveaza",
+	button_cancel : "Anuleaza",
+	button_add_tag : "+ tag",
+	button_josm : "JOSM",
+	button_add_tag_title : "Adauga un nou tag",
+	button_josm_title : "Deschide obiectul in JOSM",
+
+	title_change_properties : "Schimba proprietatile",
+	tag_addr_street : "Strada",
+	tag_addr_housenumber : "Numarul casei",
+	tag_addr_postcode : "Cod postal",
+	tag_addr_city : "Oras",
+	tag_addr_country : "Tara",
+
+	menu_login : "Conectare",
+
+	prompt_tag_name : "Va rugam, introduceti denumirea tag-ului:",
+	prompt_changeset : "Va rugam, introduceti o descriere pentru changeset:",
+
+	info_upload_successful : "Incarcare reusita",
+	info_no_object_found : "Nu s-au gasit obiecte cu probleme. Asta e bine. Felicitari!",
+	info_tokens_deleted : "S-au sters OAuth tokens",
+	info_tokens_received : "S-au primit noi OAuth tokens",
+	info_tokens_available : "OAuth  tokens disponibile.  <br>Va rugam cititi<strong>despre</strong>pentru mai multe informatii",
+
+	objects_found : "S-au gasit obiecte",
+	warn_no_localstorage : "Browserul dumneavoastra nu permite functia local storage.Nu veti putea face modificari.",
+	warn_no_changes : "Nu exista modificari disponibile pentru a fi incarcate.",
+	warn_zoom : "Va rugam, mariti mai mult pentru a descarca date. Nivelul actual de panoramare.",
+	warn_unsaved_changes : "Aveti modificari nesalvate. La descarcarea de noi date modificarile se vor pierde.",
+	error : "Eroare",
+	error_download : "Zona actuala este probabil prea mare pentru server. Dati zoom in sau reduceti dimensiunea ferestrei browserului",
+	error_jsom : "Exista o problema cu permisiunea JavaScript.Asigurati-va ca puteti executa scripturile in cadrul domeniului localhost. Niciun script activ.",
+	error_no_tokens : "Va rugam, inregistrati-va pe OSM mai intai.",
+
+	error_geo_no_access : "Accesul interzis",
+	error_geo_no_position : "Pozitia nu poate fi determinata",
+	error_geo_timeout : "Nici un rezultat dupa 120 de secunde. Va rugam incercati din nou.",
+	error_geo_no_api : "Nu exista suport Geo Location"
+
+};
