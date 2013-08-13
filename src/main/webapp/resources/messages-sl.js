25a394e552d9d5bd3995617e709412f902e499a7
Adrian Stabiszewski <as@grundid.de>
2013-08-13 13:24:26 +0200

    added slovenian language

---
+++ src/main/webapp/resources/messages-sl.js
@@ -0,0 +1,42 @@
+var MSG = {
+	button_save : "Shrani",
+	button_cancel : "Zbriši",
+	button_add_tag : "+ Tag",
+	button_josm : "JOSM",
+	button_add_tag_title : "Dodaj novo oznako",
+	button_josm_title : "Odpri trenutni predmet v JOSM",
+
+	title_change_properties : "Spremeni lastnosti",
+	tag_addr_street : "Ulica",
+	tag_addr_housenumber : "Hišna številka",
+	tag_addr_postcode : "Poštna številka",
+	tag_addr_city : "Mesto",
+	tag_addr_country : "Država",
+
+	menu_login : "Prijava",
+
+	prompt_tag_name : "Prosim, vnesite ime oznake:",
+	prompt_changeset : "Prosim, vnesite komentar za spremembe:",
+
+	info_upload_successful : "Uspešno naloženo",
+	info_no_object_found : "Ni objektov s težavami. To je dobro. Čestitke!",
+	info_tokens_deleted : "Odstranjeni OAuth žetoni",
+	info_tokens_received : "Prejeti novi OAuth žetoni",
+	info_tokens_available : "OAuth žetoni so na voljo. <br>Prosim, preberi<strong>O</strong>za več informacij",
+
+	objects_found : "najden objekt",
+	warn_no_localstorage : "Vaš brskalnik ne podpira lokalno shranjevanje. Ne morete opraviti nobenih spremeb.",
+	warn_no_changes : "Ni sprememb za prenos",
+	warn_zoom : "Prosim malo povečajte za nalaganje podatkov. Trenutna stopnja povečave",
+	warn_unsaved_changes : "Imate neshranjene spremembe. Z naložitvijo novih podatkov bodo vaše spremembe izgubljene.",
+	error : "Napaka",
+	error_download : "Verjetno je trenutno območje preveliko za server.\nPoizkusite s povečavo ali pa zmanjšajte velikost okna vašega brskalnika.",
+	error_jsom : "Težava z JavaScript dovoljenji.\mPrepričajte se, da lahko izvajate skripte pri vašem lokalnem gostitelju domene.",
+	error_no_tokens : "Prosim, prijavite se najprej z OSM.",
+
+	error_geo_no_access : "Dostop ni dovoljen.",
+	error_geo_no_position : "Ni mogoče določiti položaja.",
+	error_geo_timeout : "Po 120 sekundah ni rezultata. Prosim, poskusite znova.",
+	error_geo_no_api : "Brez podpore GEO Location"
+
+};
